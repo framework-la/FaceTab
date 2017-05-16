f84bfc865afac12de2384fe31624cce84ceb39b9
Jamin Ewell <jamin.ewell@yahoo.com>
2017-05-15 20:51:09 -0700

    returns movie info

---
+++ movieData.js
@@ -0,0 +1,412 @@
+{
+    "page": 1,
+    "results": [
+        {
+            "poster_path": "/y4MBh0EjBlMuOzv9axM4qJlmhzz.jpg",
+            "adult": false,
+            "overview": "The Guardians must fight to keep their newfound family together as they unravel the mysteries of Peter Quill's true parentage.",
+            "release_date": "2017-04-24",
+            "genre_ids": [
+                35,
+                28,
+                12,
+                878
+            ],
+            "id": 283995,
+            "original_title": "Guardians of the Galaxy Vol. 2",
+            "original_language": "en",
+            "title": "Guardians of the Galaxy Vol. 2",
+            "backdrop_path": "/aJn9XeesqsrSLKcHfHP4u5985hn.jpg",
+            "popularity": 128.89221,
+            "vote_count": 1206,
+            "video": false,
+            "vote_average": 7.8
+        },
+        {
+            "poster_path": "/tnmL0g604PDRJwGJ5fsUSYKFo9.jpg",
+            "adult": false,
+            "overview": "A live-action adaptation of Disney's version of the classic 'Beauty and the Beast' tale of a cursed prince and a beautiful young woman who helps him break the spell.",
+            "release_date": "2017-03-16",
+            "genre_ids": [
+                14,
+                10749
+            ],
+            "id": 321612,
+            "original_title": "Beauty and the Beast",
+            "original_language": "en",
+            "title": "Beauty and the Beast",
+            "backdrop_path": "/6aUWe0GSl69wMTSWWexsorMIvwU.jpg",
+            "popularity": 95.010954,
+            "vote_count": 2381,
+            "video": false,
+            "vote_average": 6.8
+        },
+        {
+            "poster_path": "/iNpz2DgTsTMPaDRZq2tnbqjL2vF.jpg",
+            "adult": false,
+            "overview": "When a mysterious woman seduces Dom into the world of crime and a betrayal of those closest to him, the crew face trials that will test them as never before.",
+            "release_date": "2017-04-12",
+            "genre_ids": [
+                28,
+                80,
+                53
+            ],
+            "id": 337339,
+            "original_title": "The Fate of the Furious",
+            "original_language": "en",
+            "title": "The Fate of the Furious",
+            "backdrop_path": "/jzdnhRhG0dsuYorwvSqPqqnM1cV.jpg",
+            "popularity": 65.235081,
+            "vote_count": 1652,
+            "video": false,
+            "vote_average": 6.8
+        },
+        {
+            "poster_path": "/y31QB9kn3XSudA15tV7UWQ9XLuW.jpg",
+            "adult": false,
+            "overview": "Light years from Earth, 26 years after being abducted, Peter Quill finds himself the prime target of a manhunt after discovering an orb wanted by Ronan the Accuser.",
+            "release_date": "2014-07-30",
+            "genre_ids": [
+                28,
+                878,
+                12
+            ],
+            "id": 118340,
+            "original_title": "Guardians of the Galaxy",
+            "original_language": "en",
+            "title": "Guardians of the Galaxy",
+            "backdrop_path": "/bHarw8xrmQeqf3t8HpuMY7zoK4x.jpg",
+            "popularity": 47.512868,
+            "vote_count": 7419,
+            "video": false,
+            "vote_average": 7.9
+        },
+        {
+            "poster_path": "/1SwAVYpuLj8KsHxllTF8Dt9dSSX.jpg",
+            "adult": false,
+            "overview": "A young black man visits his white girlfriend's cursed family estate. He finds out that many of its residents, who are black, have gone missing in the past.",
+            "release_date": "2017-02-24",
+            "genre_ids": [
+                9648,
+                53,
+                27
+            ],
+            "id": 419430,
+            "original_title": "Get Out",
+            "original_language": "en",
+            "title": "Get Out",
+            "backdrop_path": "/Ae58bf7Yj6OPzwKerPgXSnxCJdh.jpg",
+            "popularity": 44.518806,
+            "vote_count": 918,
+            "video": false,
+            "vote_average": 7.1
+        },
+        {
+            "poster_path": "/ewVHnq4lUiovxBCu64qxq5bT2lu.jpg",
+            "adult": false,
+            "overview": "Bound for a remote planet on the far side of the galaxy, the crew of the colony ship Covenant discovers what they think is an uncharted paradise, but is actually a dark, dangerous world — whose sole inhabitant is the “synthetic” David, survivor of the doomed Prometheus expedition.",
+            "release_date": "2017-05-10",
+            "genre_ids": [
+                53,
+                27,
+                878
+            ],
+            "id": 126889,
+            "original_title": "Alien: Covenant",
+            "original_language": "en",
+            "title": "Alien: Covenant",
+            "backdrop_path": "/kMU8trT43p5LFoJ4plIySMOsZ1T.jpg",
+            "popularity": 44.260045,
+            "vote_count": 153,
+            "video": false,
+            "vote_average": 5.6
+        },
+        {
+            "poster_path": "/unPB1iyEeTBcKiLg8W083rlViFH.jpg",
+            "adult": false,
+            "overview": "A story about how a new baby's arrival impacts a family, told from the point of view of a delightfully unreliable narrator, a wildly imaginative 7 year old named Tim.",
+            "release_date": "2017-03-23",
+            "genre_ids": [
+                16,
+                35,
+                10751
+            ],
+            "id": 295693,
+            "original_title": "The Boss Baby",
+            "original_language": "en",
+            "title": "The Boss Baby",
+            "backdrop_path": "/bTFeSwh07oX99ofpDI4O2WkiFJ.jpg",
+            "popularity": 43.423626,
+            "vote_count": 1043,
+            "video": false,
+            "vote_average": 5.7
+        },
+        {
+            "poster_path": "/eSVtBB2PVFbQiFWC7CQi3EjIZnn.jpg",
+            "adult": false,
+            "overview": "A koala named Buster recruits his best friend to help him drum up business for his theater by hosting a singing competition.",
+            "release_date": "2016-11-23",
+            "genre_ids": [
+                16,
+                35,
+                18,
+                10751,
+                10402
+            ],
+            "id": 335797,
+            "original_title": "Sing",
+            "original_language": "en",
+            "title": "Sing",
+            "backdrop_path": "/usJKCEMXV3tECgIJj8ZTEndmY2E.jpg",
+            "popularity": 35.501938,
+            "vote_count": 1460,
+            "video": false,
+            "vote_average": 6.8
+        },
+        {
+            "poster_path": "/myRzRzCxdfUWjkJWgpHHZ1oGkJd.jpg",
+            "adult": false,
+            "overview": "In the near future, Major is the first of her kind: a human saved from a terrible crash, who is cyber-enhanced to be a perfect soldier devoted to stopping the world's most dangerous criminals.",
+            "release_date": "2017-03-29",
+            "genre_ids": [
+                28,
+                80,
+                18,
+                9648,
+                878,
+                53
+            ],
+            "id": 315837,
+            "original_title": "Ghost in the Shell",
+            "original_language": "en",
+            "title": "Ghost in the Shell",
+            "backdrop_path": "/jGPSVArC0GS2VVc0aGAqGTjfFOG.jpg",
+            "popularity": 33.951699,
+            "vote_count": 1011,
+            "video": false,
+            "vote_average": 5.8
+        },
+        {
+            "poster_path": "/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "adult": false,
+            "overview": "Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
+            "release_date": "2014-11-05",
+            "genre_ids": [
+                12,
+                18,
+                878
+            ],
+            "id": 157336,
+            "original_title": "Interstellar",
+            "original_language": "en",
+            "title": "Interstellar",
+            "backdrop_path": "/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg",
+            "popularity": 33.782012,
+            "vote_count": 8630,
+            "video": false,
+            "vote_average": 8
+        },
+        {
+            "poster_path": "/45Y1G5FEgttPAwjTYic6czC9xCn.jpg",
+            "adult": false,
+            "overview": "In the near future, a weary Logan cares for an ailing Professor X in a hide out on the Mexican border. But Logan's attempts to hide from the world and his legacy are up-ended when a young mutant arrives, being pursued by dark forces.",
+            "release_date": "2017-02-28",
+            "genre_ids": [
+                28,
+                18,
+                878
+            ],
+            "id": 263115,
+            "original_title": "Logan",
+            "original_language": "en",
+            "title": "Logan",
+            "backdrop_path": "/5pAGnkFYSsFJ99ZxDIYnhQbQFXs.jpg",
+            "popularity": 32.82726,
+            "vote_count": 2999,
+            "video": false,
+            "vote_average": 7.5
+        },
+        {
+            "poster_path": "/rXMWOZiCt6eMX22jWuTOSdQ98bY.jpg",
+            "adult": false,
+            "overview": "Though Kevin has evidenced 23 personalities to his trusted psychiatrist, Dr. Fletcher, there remains one still submerged who is set to materialize and dominate all the others. Compelled to abduct three teenage girls led by the willful, observant Casey, Kevin reaches a war for survival among all of those contained within him — as well as everyone around him — as the walls between his compartments shatter apart.",
+            "release_date": "2016-11-15",
+            "genre_ids": [
+                27,
+                53
+            ],
+            "id": 381288,
+            "original_title": "Split",
+            "original_language": "en",
+            "title": "Split",
+            "backdrop_path": "/4G6FNNLSIVrwSRZyFs91hQ3lZtD.jpg",
+            "popularity": 28.642498,
+            "vote_count": 2389,
+            "video": false,
+            "vote_average": 6.8
+        },
+        {
+            "poster_path": "/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg",
+            "adult": false,
+            "overview": "Twenty-two years after the events of Jurassic Park, Isla Nublar now features a fully functioning dinosaur theme park, Jurassic World, as originally envisioned by John Hammond.",
+            "release_date": "2015-06-09",
+            "genre_ids": [
+                28,
+                12,
+                878,
+                53
+            ],
+            "id": 135397,
+            "original_title": "Jurassic World",
+            "original_language": "en",
+            "title": "Jurassic World",
+            "backdrop_path": "/dkMD5qlogeRMiEixC4YNPUvax2T.jpg",
+            "popularity": 28.425924,
+            "vote_count": 7137,
+            "video": false,
+            "vote_average": 6.5
+        },
+        {
+            "poster_path": "/yNsdyNbQqaKN0TQxkHMws2KLTJ6.jpg",
+            "adult": false,
+            "overview": "Extreme athlete turned government operative Xander Cage comes out of self-imposed exile, thought to be long dead, and is set on a collision course with deadly alpha warrior Xiang and his team in a race to recover a sinister and seemingly unstoppable weapon known as Pandora's Box. Recruiting an all-new group of thrill-seeking cohorts, Xander finds himself enmeshed in a deadly conspiracy that points to collusion at the highest levels of world governments.",
+            "release_date": "2017-01-13",
+            "genre_ids": [
+                28,
+                12,
+                80
+            ],
+            "id": 47971,
+            "original_title": "xXx: Return of Xander Cage",
+            "original_language": "en",
+            "title": "xXx: Return of Xander Cage",
+            "backdrop_path": "/tXuH8FOqsSbmB8p8CUN1L1cBV6d.jpg",
+            "popularity": 25.690592,
+            "vote_count": 875,
+            "video": false,
+            "vote_average": 5.5
+        },
+        {
+            "poster_path": "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
+            "adult": false,
+            "overview": "An apocalyptic story set in the furthest reaches of our planet, in a stark desert landscape where humanity is broken, and most everyone is crazed fighting for the necessities of life. Within this world exist two rebels on the run who just might be able to restore order. There's Max, a man of action and a man of few words, who seeks peace of mind following the loss of his wife and child in the aftermath of the chaos. And Furiosa, a woman of action and a woman who believes her path to survival may be achieved if she can make it across the desert back to her childhood homeland.",
+            "release_date": "2015-05-13",
+            "genre_ids": [
+                28,
+                12,
+                878,
+                53
+            ],
+            "id": 76341,
+            "original_title": "Mad Max: Fury Road",
+            "original_language": "en",
+            "title": "Mad Max: Fury Road",
+            "backdrop_path": "/phszHPFVhPHhMZgo0fWTKBDQsJA.jpg",
+            "popularity": 25.660661,
+            "vote_count": 7833,
+            "video": false,
+            "vote_average": 7.2
+        },
+        {
+            "poster_path": "/hLudzvGfpi6JlwUnsNhXwKKg4j.jpg",
+            "adult": false,
+            "overview": "Taking place after alien crafts land around the world, an expert linguist is recruited by the military to determine whether they come in peace or are a threat.",
+            "release_date": "2016-11-10",
+            "genre_ids": [
+                53,
+                18,
+                878,
+                9648
+            ],
+            "id": 329865,
+            "original_title": "Arrival",
+            "original_language": "en",
+            "title": "Arrival",
+            "backdrop_path": "/yIZ1xendyqKvY3FGeeUYUd5X9Mm.jpg",
+            "popularity": 24.376128,
+            "vote_count": 3948,
+            "video": false,
+            "vote_average": 7
+        },
+        {
+            "poster_path": "/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
+            "adult": false,
+            "overview": "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.",
+            "release_date": "2014-10-22",
+            "genre_ids": [
+                28,
+                53
+            ],
+            "id": 245891,
+            "original_title": "John Wick",
+            "original_language": "en",
+            "title": "John Wick",
+            "backdrop_path": "/mFb0ygcue4ITixDkdr7wm1Tdarx.jpg",
+            "popularity": 22.73836,
+            "vote_count": 4020,
+            "video": false,
+            "vote_average": 6.9
+        },
+        {
+            "poster_path": "/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
+            "adult": false,
+            "overview": "Based upon Marvel Comics’ most unconventional anti-hero, DEADPOOL tells the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life.",
+            "release_date": "2016-02-09",
+            "genre_ids": [
+                28,
+                12,
+                35,
+                10749
+            ],
+            "id": 293660,
+            "original_title": "Deadpool",
+            "original_language": "en",
+            "title": "Deadpool",
+            "backdrop_path": "/n1y094tVDFATSzkTnFxoGZ1qNsG.jpg",
+            "popularity": 21.647178,
+            "vote_count": 8324,
+            "video": false,
+            "vote_average": 7.3
+        },
+        {
+            "poster_path": "/kSBXou5Ac7vEqKd97wotJumyJvU.jpg",
+            "adult": false,
+            "overview": "Following the events of Age of Ultron, the collective governments of the world pass an act designed to regulate all superhuman activity. This polarizes opinion amongst the Avengers, causing two factions to side with Iron Man or Captain America, which causes an epic battle between former allies.",
+            "release_date": "2016-04-27",
+            "genre_ids": [
+                12,
+                28,
+                878
+            ],
+            "id": 271110,
+            "original_title": "Captain America: Civil War",
+            "original_language": "en",
+            "title": "Captain America: Civil War",
+            "backdrop_path": "/m5O3SZvQ6EgD5XXXLPIP1wLppeW.jpg",
+            "popularity": 21.507,
+            "vote_count": 5597,
+            "video": false,
+            "vote_average": 6.9
+        },
+        {
+            "poster_path": "/gri0DDxsERr6B2sOR1fGLxLpSLx.jpg",
+            "adult": false,
+            "overview": "In 1926, Newt Scamander arrives at the Magical Congress of the United States of America with a magically expanded briefcase, which houses a number of dangerous creatures and their habitats. When the creatures escape from the briefcase, it sends the American wizarding authorities after Newt, and threatens to strain even further the state of magical and non-magical relations.",
+            "release_date": "2016-11-16",
+            "genre_ids": [
+                14
+            ],
+            "id": 259316,
+            "original_title": "Fantastic Beasts and Where to Find Them",
+            "original_language": "en",
+            "title": "Fantastic Beasts and Where to Find Them",
+            "backdrop_path": "/kMzU4PkXcKcDMngCxXji0BbVXsu.jpg",
+            "popularity": 21.100602,
+            "vote_count": 3753,
+            "video": false,
+            "vote_average": 7.1
+        }
+    ],
+    "total_results": 310873,
+    "total_pages": 15544
+}
--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
-(function(d, s, id) {
-  d.getElementsByTagName('p')[0].style.color = 'red';
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1346890432070427";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+var url = 'https://api.themoviedb.org/3/movie/157336?api_key=78e5abe0311622cd1bfea829dac9843e&append_to_response=videos';
+
+
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    success: function (data) {
+    console.log(data);
+    }
+  })
